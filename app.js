5c902f96ec143ddc01776aceb834cb916386b379
Benjamin Dumas <benjamindum@gmail.com>
2016-10-09 19:04:13 +0200

    Ajout de la fonction googleCal

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 var restify = require('restify');
 var builder = require('botbuilder');
+var FS = require("q-io/fs");
+var HTTP = require('q-io/http');
+
+var googleCalHandler = require( './googleCal/calfunction' );
+
+var intents = new builder.IntentDialog();
 
 //=========================================================
 // Bot Setup
@@ -19,17 +25,64 @@ var connector = new builder.ChatConnector({
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
-//=========================================================
-// Bots Dialogs
-//=========================================================
-
-bot.dialog('/', function (session) {
-    session.send("Hello Benj");
-});
-
 // Serve a static web page
 server.get(/.*/, restify.serveStatic({
 	'directory': '.',
 	'default': 'index.html'
 }));
 
+
+//=========================================================
+// Bots Dialogs
+//=========================================================
+
+
+bot.dialog('/', intents);
+
+intents.matches(/^change profile/i, [
+    function (session) {
+        session.beginDialog('/profile');
+    },
+    function (session, results) {
+        session.send('Ok... Changed your name to %s', session.userData.name);
+    }
+]);
+
+intents.matches(/^cal/i, [
+    function (session) {
+        session.beginDialog('/google');
+    }
+]);
+
+intents.onDefault([
+    function (session, args, next) {
+        if (!session.userData.name) {
+            session.beginDialog('/profile');
+        } else {
+            next();
+        }
+    },
+    function (session, results) {
+        session.send('Hello %s!', session.userData.name);
+    }
+]);
+
+bot.dialog('/profile', [
+    function (session) {
+        builder.Prompts.text(session, 'Hi! What is your name?');
+    },
+    function (session, results) {
+        session.userData.name = results.response;
+        session.endDialog();
+    }
+]);
+
+bot.dialog('/google', [
+    function (session) {
+        session.send('Laisse moi 5 secondes');
+        session.send('Je demande à Google !');
+        googleCalHandler.init(session);
+        
+        
+    }
+]);
