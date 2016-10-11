2e13fc50d5a1f0e92a87138a6e4fac1cb5cf1b51
Benjamin Dumas <benjamindum@gmail.com>
2016-10-11 21:18:38 +0200

    Fix

--- app.js
+++ app.js
@@ -113,29 +113,6 @@ bot.dialog('/google', [
 ]);
 
 
-bot.dialog('/', [
-    function (session, args, next) {
-        if (!session.userData.name) {
-            session.beginDialog('/profile');
-        } else {
-            next();
-        }
-    },
-    function (session, results) {
-        session.send('Hello %s!', session.userData.name);
-    }
-]);
-
-bot.dialog('/profile', [
-    function (session) {
-        builder.Prompts.text(session, 'Hi! What is your name?');
-    },
-    function (session, results) {
-        session.userData.name = results.response;
-        session.endDialog();
-    }
-]);
-
 bot.dialog('/googleUpdated',[
     function (session) {
         //session.send('Laisse moi 5 secondes');
